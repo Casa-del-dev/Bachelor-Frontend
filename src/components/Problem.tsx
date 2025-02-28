46aca861a533f3a3749fbc8fe921b6b16ee097f9
Eren Homburg <ehomburg@student.ethz.ch>
2025-02-28 16:24:19 +0100

    finally solved the header problem (made a function) and did the layout for problem

--- src/components/Problem.tsx
+++ src/components/Problem.tsx
@@ -1,7 +1,27 @@
 import "./Problem.css";
 
 const Problem = () => {
-  return <div className="container">Problem</div>;
+  return (
+    <div className="container">
+      {/*Left side*/}
+      <div className="left-side">
+        <div className="container-fluid">Problem</div>
+        <div className="container-fluid">Problem</div>
+        <div className="container-fluid">Problem</div>
+        <div className="container-fluid">Problem</div>
+      </div>
+      {/*Separator*/}
+      <div className="custom-line"></div>
+
+      {/*Right side*/}
+      <div className="right-side">
+        <div className="container-fluid">Problem</div>
+        <div className="container-fluid">Problem</div>
+        <div className="container-fluid">Problem</div>
+        <div className="container-fluid">Problem</div>
+      </div>
+    </div>
+  );
 };
 
 export default Problem;
