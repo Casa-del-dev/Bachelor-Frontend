26567be321a7959aa2497e4bcaf4feae46a6ffe0
Eren Homburg <ehomburg@student.ethz.ch>
2025-03-02 13:43:29 +0100

    fixed header now

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,30 +1,7 @@
-import { useEffect } from "react";
 import Logo from "../assets/Peachlab_logo.png";
 import "./Header.css";
 
 export function Header() {
-  useEffect(() => {
-    function updateHeaderHeight() {
-      const header = document.querySelector(".header") as HTMLElement | null;
-      if (header) {
-        const headerHeight = `${header.offsetHeight}px`;
-        document.documentElement.style.setProperty(
-          "--header-height",
-          headerHeight
-        );
-      }
-    }
-
-    // Run function on load and resize
-    updateHeaderHeight();
-    window.addEventListener("resize", updateHeaderHeight);
-
-    // Cleanup event listener on unmount
-    return () => {
-      window.removeEventListener("resize", updateHeaderHeight);
-    };
-  }, []);
-
   return (
     <header className="header">
       <div className="header-left">
