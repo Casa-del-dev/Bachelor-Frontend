932b6eb0d8f22bd5106c1c42d28f5b0badad78ff
Eren Homburg <ehomburg@student.ethz.ch>
2025-03-02 15:43:26 +0100

    did the Buttons for the problem page

--- src/components/Problem.tsx
+++ src/components/Problem.tsx
@@ -1,17 +1,39 @@
 import "./Problem.css";
+//import Button from "./BuildingBlocks/Button.tsx";
 
 const Problem = () => {
   return (
     <div className="container">
       {/*Left side*/}
       <div className="left-side">
-        <div className="container-fluid">Problem</div>
-        <div className="container-fluid">Problem</div>
-        <div className="container-fluid">Problem</div>
-        <div className="container-fluid">Problem</div>
+        <div className="left-side-title">Problems</div>
+        <div className="custom-line-left-side"></div>
+        <div className="left-side-content">
+          <div className="general-button">Problem 1</div>
+          <div className="general-button">Problem 2</div>
+          <div className="general-button">Problem 3</div>
+          <div className="general-button">Problem 4</div>
+          <div className="general-button">Problem 5</div>
+          <div className="general-button">Problem 6</div>
+          <div className="general-button">Problem 7</div>
+          <div className="general-button">Problem 8</div>
+          <div className="general-button">Problem 9</div>
+          <div className="general-button">Problem 9</div>
+          <div className="general-button">Problem 9</div>
+          <div className="general-button">Problem 9</div>
+          <div className="general-button">Problem 9</div>
+          <div className="general-button">Problem 9</div>
+          <div className="general-button">Problem 9</div>
+          <div className="general-button">Problem 9</div>
+          <div className="general-button">Problem 9</div>
+          <div className="general-button">Problem 9</div>
+          <div className="general-button">Problem 9</div>
+        </div>
       </div>
       {/*Separator*/}
-      <div className="custom-line"></div>
+      <div className="container-separator-problem">
+        <div className="custom-line"></div>
+      </div>
 
       {/*Right side*/}
       <div className="right-side">
