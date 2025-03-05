c84518a3b70c6c961492eb86fb52881d1f4aa234
Eren Homburg <ehomburg@student.ethz.ch>
2025-03-05 21:45:42 +0100

    made sure the the left part now displays the right thing!

--- src/components/Start-left.tsx
+++ src/components/Start-left.tsx
@@ -1,26 +1,74 @@
+import React, { useState } from "react";
 import "./Start-left.css";
 
-const startLeft = () => {
+const StartLeft = () => {
+  // "Problem" is the default selected section
+  const [selectedSection, setSelectedSection] = useState<
+    "Project" | "Problem" | "Blocks"
+  >("Problem");
+
+  const openProjectFiles = () => {
+    setSelectedSection("Project");
+  };
+
+  const openProblem = () => {
+    setSelectedSection("Problem");
+  };
+
+  const openBuildingBlocks = () => {
+    setSelectedSection("Blocks");
+  };
+
+  // Render the chosen content
+  const renderContent = () => {
+    switch (selectedSection) {
+      case "Project":
+        return <div>Project System Files Content</div>;
+      case "Problem":
+        return <div>Problem Content</div>;
+      case "Blocks":
+        return <div>Building Blocks Content</div>;
+      default:
+        return <div>Problem Content</div>;
+    }
+  };
+
   return (
     <div className="left-main">
       <div className="left-content-main">
         <div className="class-created-for-seperator">
           <div className="header-left-main">
-            <div className="Project-system-files">
+            {/* Three "buttons" */}
+            <div
+              className="Project-system-files invisible-button"
+              onClick={openProjectFiles}
+            >
               Project <br /> system files
             </div>
-            <div className="Problem-description-main">Problem</div>
-            <div className="Building-blocks">
+            <div className="custom-separator-for-start-purpose"></div>
+            <div
+              className="Problem-description-main invisible-button"
+              onClick={openProblem}
+            >
+              Problem
+            </div>
+            <div className="custom-separator-for-start-purpose"></div>
+            <div
+              className="Building-blocks invisible-button"
+              onClick={openBuildingBlocks}
+            >
               Building <br /> Blocks
             </div>
           </div>
           <div className="custom-line-horizontal"></div>
         </div>
-        <div className="all-type-of-content">content</div>
+
+        {/* Dynamic section - will show whichever is selected */}
+        <div className="all-type-of-content">{renderContent()}</div>
       </div>
       <div className="custom-line-leftmiddle"></div>
     </div>
   );
 };
 
-export default startLeft;
+export default StartLeft;
